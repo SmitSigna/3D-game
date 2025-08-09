3ccc07cc726e4672ddf552820a84354845cc5557
Smit Signapurkar <smitsignapurkar@gmail.com>
2025-08-09 23:55:15 +0530

    Added some animations

---
+++ Assets/Scripts/EnemyAI.cs
@@ -0,0 +1,130 @@
+using UnityEngine;
+using UnityEngine.AI;
+
+
+public class EnemyAI : MonoBehaviour
+{
+    public NavMeshAgent agent;
+
+    public Transform player;
+
+    public LayerMask whatIsGround, whatIsPlayer;
+
+    Animator anim;
+
+    //Patroling
+    public Vector3 walkPoint;
+    bool walkPointSet;
+    public float walkPointRange;
+
+    //Attacking
+    public float timeBetweenAttacks;
+    bool alreadyAttacked;
+
+    //States
+    public float sightRange, attackRange;
+    public bool playerInSightRange, playerInAttackRange;
+
+    void Awake()
+    {
+        player = GameObject.Find("FPS Player").transform;
+        agent = GetComponent<NavMeshAgent>();
+        anim = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        //Check for sight and attack range
+        playerInSightRange = Physics.CheckSphere(transform.position, sightRange, whatIsPlayer);
+        playerInAttackRange = Physics.CheckSphere(transform.position, attackRange, whatIsPlayer);
+
+        if (!playerInAttackRange && !playerInSightRange)
+        {
+            Patroling();
+        }
+
+        if (playerInSightRange && !playerInAttackRange)
+        {
+            ChasePlayer();
+        }
+
+        if (playerInSightRange && playerInAttackRange)
+        {
+            AttackPlayer();
+        }
+    }
+
+    private void Patroling()
+    {
+        anim.SetBool("isWalking", true);
+        anim.SetBool("isRunning", false);
+        anim.SetBool("isAttacking", false);
+
+        if (!walkPointSet)
+        {
+            SearchWalkPoint();
+        }
+
+        if (walkPointSet)
+        {
+            agent.SetDestination(walkPoint);
+        }
+
+        Vector3 distanceToWalkPoint = transform.position - walkPoint;
+
+        //Walk poin reached
+        if (distanceToWalkPoint.magnitude < 1f)
+        {
+            walkPointSet = false;
+        }
+    }
+
+    private void SearchWalkPoint()
+    {
+        //Calculate rondom point in range
+        float randomZ = Random.Range(-walkPointRange, walkPointRange);
+        float randomX = Random.Range(-walkPointRange, walkPointRange);
+
+        walkPoint = new Vector3(transform.position.x + randomX, transform.position.y, transform.position.z + randomZ);
+
+        if (Physics.Raycast(walkPoint, -transform.up, 2f, whatIsGround))//to check it the walk point is not outside the map
+        {
+            walkPointSet = true;
+        }
+    }
+
+    private void ChasePlayer()
+    {
+        anim.SetBool("isWalking", false);
+        anim.SetBool("isRunning", true);
+        anim.SetBool("isAttacking", false);
+
+
+        agent.SetDestination(player.position);
+    }
+    private void AttackPlayer()
+    {
+        anim.SetBool("isWalking", false);
+        anim.SetBool("isRunning", false);
+        anim.SetBool("isAttacking", true);
+
+        //Make sure enemy doesnot move
+        agent.SetDestination(transform.position);
+
+        transform.LookAt(player);
+
+        if (!alreadyAttacked)
+        {
+            //Attack code here
+
+
+            //
+            alreadyAttacked = true;
+            Invoke(nameof(ResetAttack), timeBetweenAttacks);
+        }
+    }
+    private void ResetAttack()
+    {
+        alreadyAttacked = false;
+    }
+}
