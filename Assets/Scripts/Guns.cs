59536dfadf6e8ec526e36d061616544ada14fd66
Smit Signapurkar <smitsignapurkar@gmail.com>
2025-08-19 17:49:13 +0530

    final commit

--- Assets/Scripts/Guns.cs
+++ Assets/Scripts/Guns.cs
@@ -3,18 +3,21 @@ using UnityEngine.InputSystem;
 
 public class Guns : MonoBehaviour
 {
-    
+
     [SerializeField] float damage = 10f;
     [SerializeField] float range = 100f;
     [SerializeField] float fireRate = 30f;
     [SerializeField] Camera fpsCam;
 
     [SerializeField] ParticleSystem muzzleFlash;
+
+
     private float nextTimeToFire = 0f;
+
 
     void Update()
     {
-        if ( Input.GetMouseButton(0) && Time.time >= nextTimeToFire )
+        if (Input.GetMouseButton(0) && Time.time >= nextTimeToFire)
         {
             nextTimeToFire = Time.time + (1f / fireRate);
             Shoot();
@@ -23,20 +26,28 @@ public class Guns : MonoBehaviour
 
     void Shoot()
     {
-        muzzleFlash.Play();
+        if (muzzleFlash.isPlaying)
+        {
+            muzzleFlash.Stop();
+        }
+        else
+        {
+            muzzleFlash.Play();
+        }
+
         RaycastHit hit;
         if (Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward, out hit, range))
         {
-           // Debug.Log(hit.transform.name);
+            // Debug.Log(hit.transform.name);
             Target target = hit.transform.GetComponent<Target>();
 
             if (target != null)
             {
                 target.TakeDamage(damage);
             }
-           
+
         }
 
 
     }
-}   
+}
